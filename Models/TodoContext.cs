ca5f46fcb80e896782a63eb1acb98b5eaf1e1b8f
Yesid Balvin <ybalvins@powerplan.com>
2020-01-28 09:43:44 -0500

    Add project files.

---
+++ Models/TodoContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace aspnetcore_test.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+             : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
